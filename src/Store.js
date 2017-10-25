9200b1f48a1e079b0e05a7854c1bcf29cc0ca29b
adam_giacomelli <adam.giacom@gmail.com>
2017-10-25 20:35:12 +0200

    Added autoplay

--- src/Store.js
+++ src/Store.js
@@ -1,5 +1,7 @@
 import { extendObservable } from 'mobx'
 import { getPhotos } from './api/Unsplash'
+import { CONFIG } from './constants/Config'
+
 
 class Store {
   constructor () {
@@ -7,26 +9,47 @@ class Store {
       images: [],
       nextPageIndex: 0,
       currentImageOffset: 0,
-      photosPerPage: 10
+      photosPerPage: 10,
+      autoplayCountdown: CONFIG.AUTOPLAY_DELAY, 
     })
+    
+    setInterval(() => this.autoplay(), 1000);
+  }
+
+  autoplay() {
+    if(this.autoplayCountdown === 0) {
+      this.checkForLoadMore()
+      this.currentImageOffset--
+    } else {
+      this.autoplayCountdown--
+    }
   }
   
   async loadPhotos () {
     const photos = await getPhotos(this.nextPageIndex + 1)
-    console.log(photos)
     this.images.push(...photos)
     this.nextPageIndex++
   }
 
   incrementIndex () {    
     this.currentImageOffset++
+    this.resetAutoplay()
   }
 
   decrementIndex () {    
+    this.checkForLoadMore()
+    this.currentImageOffset--
+    this.resetAutoplay()
+  }
+
+  checkForLoadMore() {
     if(this.nextPageIndex*this.photosPerPage + this.currentImageOffset < this.photosPerPage) {
       this.loadPhotos();
     }
-    this.currentImageOffset--
+  }
+
+  resetAutoplay() {
+    this.autoplayCountdown = CONFIG.AUTOPLAY_DELAY;
   }
 }
 
---
+++ src/constants/Config.js
@@ -0,0 +1,4 @@
+
+export const CONFIG = {
+  AUTOPLAY_DELAY: 10
+}
